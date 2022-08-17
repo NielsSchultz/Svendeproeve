5560085dbb46cf8b48ce4a7b9ad3983cb67d2c31
Joan Krarup Langgaard <joan@nordigo.com>
2022-08-17 15:16:00 +0200

    added DTOs to common project

---
+++ RegionSyd.Common/Models/PatientDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RegionSyd.Common.Models
+{
+    public partial class Patient
+    {
+        public Patient()
+        {
+            Journals = new HashSet<Journal>();
+            Monitors = new HashSet<Monitor>();
+        }
+
+        public int PatientId { get; set; }
+        public int UserId { get; set; }
+        public int Cpr { get; set; }
+        public DateTime Birthday { get; set; }
+
+        public virtual User User { get; set; } = null!;
+        public virtual ICollection<Journal> Journals { get; set; }
+        public virtual ICollection<Monitor> Monitors { get; set; }
+    }
+}
---
+++ RegionSyd.Common/Models/TreatmentDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RegionSyd.Common.Models
+{
+    public partial class Treatment
+    {
+        public Treatment()
+        {
+            Bookings = new HashSet<Booking>();
+        }
+
+        public int TreatmentId { get; set; }
+        public int DepartmentId { get; set; }
+        public string TreatmentName { get; set; } = null!;
+        public TimeSpan TreatmentDuration { get; set; }
+
+        public virtual Department Department { get; set; } = null!;
+        public virtual ICollection<Booking> Bookings { get; set; }
+    }
+}
