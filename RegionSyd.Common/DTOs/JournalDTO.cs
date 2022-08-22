d4c047fa635863d232fae120e2ec334a9872cf1c
Joan Krarup Langgaard <joan@nordigo.com>
2022-08-22 10:46:55 +0200

    removed  ;

--- RegionSyd.Common/DTOs/JournalDTO.cs
+++ RegionSyd.Common/DTOs/JournalDTO.cs
@@ -6,7 +6,7 @@
         public int UserId { get; set; }
         public string FirstName { get; set; }
         public string? MiddleName { get; set; }
-        public string LastName { get; set; };
+        public string LastName { get; set; }
         public int PatientId { get; set; }
     }
 }
