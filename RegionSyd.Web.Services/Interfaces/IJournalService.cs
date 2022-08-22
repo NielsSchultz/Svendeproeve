dc746d477ab945b1ee7b2619622076dbf054998b
Joan Krarup Langgaard <joan@nordigo.com>
2022-08-22 10:45:11 +0200

    journal note changes

---
+++ RegionSyd.Web.Services/Interfaces/IJournalService.cs
@@ -0,0 +1,11 @@
+﻿using RegionSyd.Common.DTOs;
+
+namespace RegionSyd.Web.Services.Interfaces
+{
+    public interface IJournalService
+    {
+        Task<JournalDTO> GetJournalById(int patientId);
+        Task<JournalEntryDTO> SaveJournalEntry(JournalEntryDTO JournalEntryDTO);
+        Task<string> TestTreatment();
+    }
+}
