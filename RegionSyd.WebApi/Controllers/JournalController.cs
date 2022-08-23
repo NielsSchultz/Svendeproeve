2dc7b59fb000055538754e84b7bcadba11de0184
NielsSchultz <53754565+NielsSchultz@users.noreply.github.com>
2022-08-23 13:04:07 +0200

    added functions for journal to service and controller

--- RegionSyd.WebApi/Controllers/JournalController.cs
+++ RegionSyd.WebApi/Controllers/JournalController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RegionSyd.Common.DTOs;
+using RegionSyd.WebApi.Services.Interfaces;
 
 namespace RegionSyd.WebApi.Controllers
 {
@@ -7,5 +9,37 @@ namespace RegionSyd.WebApi.Controllers
     [ApiController]
     public class JournalController : ControllerBase
     {
+        private readonly IJournalService _journalService;
+
+        public JournalController(IJournalService journalService)
+        {
+            _journalService = journalService;
+        }
+        [HttpGet]
+        public async Task<List<JournalDTO>> GetAllJournals()
+        {
+            return await _journalService.GetJournals();
+        }
+        [HttpGet("{id}")]
+        public async Task<JournalDTO> GetJournal(int id)
+        {
+            return await _journalService.GetJournalByPatientID(id);
+        }
+        [HttpPost]
+        public async Task<JournalDTO> CreateJournal(JournalDTO journal)
+        {
+            return await _journalService.CreateJournal(journal);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<bool> DeleteJournal(int id)
+        {
+            return await _journalService.DeleteJournal(id);
+        }
+        [HttpPut]
+        public async Task<JournalDTO> UpdateJournal(JournalDTO journal)
+        {
+            return await _journalService.UpdateJournal(journal);
+        }
     }
 }
