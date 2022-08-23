cc05a2b5100068317bc3dc57971988182ef59c73
NielsSchultz <53754565+NielsSchultz@users.noreply.github.com>
2022-08-23 08:06:40 +0200

    added bed functions to repository and webapiservices

---
+++ RegionSyd.WebApi.Services/Interfaces/IBedService.cs
@@ -0,0 +1,19 @@
+﻿using RegionSyd.Common.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegionSyd.WebApi.Services.Interfaces
+{
+    public interface IBedService
+    {
+        Task<List<BedDTO>> GetBeds();
+        Task<BedDTO> GetBedById(int id);
+        Task<BedDTO> CreateBed(BedDTO bed);
+        Task<BedDTO> UpdateBed(BedDTO bed);
+        Task<bool> DeleteBed(int id);
+        Task<List<BedDTO>> GetUnoccupiedBeds();
+    }
+}
--- Repository/Interfaces/IBedRepository.cs
+++ Repository/Interfaces/IBedRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RegionSyd.Repositories.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,11 @@ namespace RegionSyd.Repositories.Interfaces
 {
     public interface IBedRepository
     {
+        Task<List<Bed>> GetBeds();
+        Task<Bed> GetBedById(int id);
+        Task<Bed> CreateBed(Bed bed);
+        Task<Bed> UpdateBed(Bed bed);
+        Task<bool> DeleteBed(int id);
+        Task<List<Bed>> GetUnoccupiedBeds();
     }
 }
