40bdcd6853ded1ed5899ff6d3a5779b65ec14f4e
NielsSchultz <53754565+NielsSchultz@users.noreply.github.com>
2022-08-23 08:23:44 +0200

    added bed controller

---
+++ RegionSyd.WebApi/Controllers/BedController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegionSyd.Common.DTOs;
+using RegionSyd.WebApi.Services.Interfaces;
+
+namespace RegionSyd.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BedController : ControllerBase
+    {
+        private readonly IBedService _bedService;
+
+        public BedController(IBedService bedService)
+        {
+            _bedService = bedService;
+        }
+        [HttpGet]
+        public async Task<List<BedDTO>> GetAllBeds()
+        {
+            return await _bedService.GetBeds();
+        }
+        [HttpGet("{id}")]
+        public async Task<BedDTO> GetBed(int id)
+        {
+            return await _bedService.GetBedById(id);
+        }
+        [HttpPost]
+        public async Task<BedDTO> CreateBed(BedDTO bed)
+        {
+            return await _bedService.CreateBed(bed);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<bool> DeleteBed(int id)
+        {
+            return await _bedService.DeleteBed(id);
+        }
+        [HttpPut]
+        public async Task<BedDTO> UpdateBed(BedDTO bed)
+        {
+            return await _bedService.UpdateBed(bed);
+        }
+    }
+}
