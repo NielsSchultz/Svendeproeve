69e747c88f658bc7ba19eebb0eaada08d827a2c3
Christian Andresen <37537862+CSAndresen@users.noreply.github.com>
2022-08-24 08:33:12 +0200

    Finished all interfaces

---
+++ Repository/Interfaces/IUserTypeRepository.cs
@@ -0,0 +1,27 @@
+﻿using RegionSyd.Repositories.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegionSyd.Repositories.Interfaces
+{
+    public interface IUserTypeRepository
+    {
+        // Create new UserType
+        Task<UserType> CreateUserType(UserType newUserType);
+
+        // Get all UserTypes
+        Task<List<UserType>> GetUserTypes();
+
+        // Get UserType by ID
+        Task<UserType> GetUserType(int id);
+
+        // Update UserType
+        Task<UserType> UpdateUserType(UserType newUserType);
+
+        // Delete UserType
+        Task<bool> DeleteUserType(int id);
+    }
+}
