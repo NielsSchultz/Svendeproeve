67b2f3e4a6e003a16e80f99d07fcf334d7de9902
NielsSchultz <53754565+NielsSchultz@users.noreply.github.com>
2022-08-25 15:35:03 +0200

    inprogress

---
+++ RegionSyd.WebApi.Services/Interfaces/IUserService.cs
@@ -0,0 +1,31 @@
+﻿using RegionSyd.Common.DTOs;
+using RegionSyd.Repositories.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegionSyd.WebApi.Services.Interfaces
+{
+    public interface IUserService
+    {
+        // Create new User
+        Task<UserDTO> CreateUser(UserDTO userDTO);
+
+        // Get all Users
+        Task<List<UserDTO>> GetUsers();
+
+        // Get User by ID
+        Task<UserDTO> GetUser(int id);
+
+        // Get User by patient ID
+        Task<UserDTO> GetUserByPatientID(int id);
+
+        // Update User
+        Task<UserDTO> UpdateUser(UserDTO userDTO);
+
+        // Delete User
+        Task<bool> DeleteUser(int id);
+    }
+}
---
+++ RegionSyd.WebApi.Services/Services/UserService.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using RegionSyd.Common.DTOs;
+using RegionSyd.Repositories.Entities;
+using RegionSyd.Repositories.Interfaces;
+using RegionSyd.WebApi.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegionSyd.WebApi.Services.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _treatmentRepository;
+        private readonly IMapper _mapper;
+
+        public UserService(IUserRepository userRepository, IMapper mapper)
+        {
+            _treatmentRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+            _mapper = mapper;
+        }
+        public async Task<User> CreateUser(UserDTO userDTO)
+        {
+            var user = _mapper.Map<User>(userDTO);
+            var returnUser = await _treatmentRepository.CreateUser(user);
+            return _mapper.Map<UserDTO>(returnUser);
+        }
+        public async Task<List<User>> GetUsers()
+        {
+
+        }
+        public async Task<User> GetUser(int id)
+        {
+
+        }
+        public async Task<User> GetUserByPatientID(int id)
+        {
+
+        }
+        public async Task<User> UpdateUser(User userDTO)
+        {
+
+        }
+        public async Task<bool> DeleteUser(int id)
+        {
+
+        }
+    }
+}
