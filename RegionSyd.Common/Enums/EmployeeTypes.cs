72036caf949ab21353badfa94005950249690725
Joan Krarup Langgaard <joan@nordigo.com>
2022-09-01 15:24:06 +0200

    added functions for approving journal notes

---
+++ RegionSyd.Common/Enums/EmployeeTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegionSyd.Common.Enums
+{
+    public enum EmployeeTypes
+    {
+        Hospitals_Læge = 1,
+        Praktiserende_Læge = 2,
+        Sygeplejerske = 3,
+        Sundhedshjælper = 4
+    }
+}
